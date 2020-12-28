282e859bdca728193f9db3bd3dec68498ad43f8a
shortland <ilankleiman@gmail.com>
2020-12-28 15:17:06 -0500

    feat: fetch quote data async & begin impl. prometheus metrics

--- LastPriceSaver/main.py
+++ LastPriceSaver/main.py
@@ -7,17 +7,19 @@ import datetime
 
 import tdameritrade
 import mysql.connector
+import prometheus_client
 
 from LastPriceSaver.config.env import (
     QUOTE_SYMBOLS,
     DATA_PATH,
     API_KEY,
     TOKEN_PATH,
-    MYSQL_ROOT_PASSWORD
+    MYSQL_ROOT_PASSWORD,
+    PROMETHEUS_PORT
 )
+from LastPriceSaver.utils.time import current_milli_time
 from LastPriceSaver.utils.logger import logger
 from LastPriceSaver.utils.secrets_reader import read_refresh_token
-from LastPriceSaver.utils.time import current_milli_time
 
 
 def main() -> None:
@@ -26,6 +28,9 @@ def main() -> None:
         client_id=API_KEY,
         refresh_token=read_refresh_token(TOKEN_PATH)
     )
+
+    logger.debug("Starting prometheus metrics server")
+    prometheus_client.start_http_server(PROMETHEUS_PORT)
 
     while True:
         start_time = current_milli_time()
@@ -39,13 +44,15 @@ def main() -> None:
             time.sleep(60)
             continue
 
-        try:
-            quotes = client.quote(QUOTE_SYMBOLS)
-            asyncio.run(write_quotes_to_db(quotes, start_time, time_sec))
-        except Exception as error:
-            logger.error(
-                "Unable to get quote data for symbols: {}".format(error)
+        logger.debug("Getting quotes data")
+        asyncio.run(
+            get_and_save_quotes(
+                client,
+                QUOTE_SYMBOLS,
+                start_time,
+                time_sec
             )
+        )
 
         """ Sleeping Calculations """
         actual_sleep = 1.0 - ((current_milli_time() - start_time) / 1000)
@@ -60,11 +67,28 @@ def main() -> None:
         time.sleep(actual_sleep)
 
 
+async def get_and_save_quotes(client: tdameritrade.TDClient, quotes, start_time: float, time_sec: int) -> None:
+    """
+    Get the quotes and then save them to DB.
+    """
+
+    try:
+        quotes = client.quote(QUOTE_SYMBOLS)
+
+        await write_quotes_to_db(quotes, start_time, time_sec)
+    except Exception as error:
+        logger.error(
+            "Unable to get quote data for symbols: {}".format(error)
+        )
+
+
 async def write_quotes_to_db(quotes, time_milli: float, time_sec: int) -> None:
     """
     Eventually either use a queue or refresh login details as necessary...
     It's excessive to login each time.
     """
+
+    logger.debug("Beginning save data to db")
 
     try:
         db = mysql.connector.connect(
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# Last Price Sec Saver
+# LPS - Last Price Saver
 
-Gets specified stock prices each second & saves it to a mysql database.
+Gets specified stock prices every second & saves it to a mysql database.
 
-I can't find reliable data like this anywhere online...
+I can't find 'free' reliable data like this online...
+
 Usually this data is available as "tick" data... Or if it is available as per "second" data, there's often times huge gaps missing every few minutes... Which just isn't sufficient for my needs.
-Realistically, you can only query the API every ~1sec (I don't own a high frequency firm with microsecond latency to stock exchange servers, so going down to the 'tick' is unrealistic, and unnecessary)... Thus "tick" data isn't sufficient, thus the development of this application.
+
+Realistically, you can only query the API every ~1sec (I don't own a high frequency firm with microsecond latency to stock exchange servers, so going down to the 'tick' is unrealistic, and unnecessary)...
